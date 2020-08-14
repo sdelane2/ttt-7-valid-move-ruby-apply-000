1b1e005f9560bc3a29b9a5df1967971decabc3c6
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-14 22:23:05 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,11 @@
-
-
+def position_taken?(board, index)
+  if board[index] == (" ") || board[index] == ("") || board[index] == nil
+    return FALSE
+  else
+    return TRUE
+  end
+end
 def valid_move?(board, index)
+  if position_taken?
+    return FALSE
+  elsif index.between
